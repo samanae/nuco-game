982a4860c7024293db4766003ca7940641ffeee3
samanae <samia.rachid03@gmail.com>
2024-10-13 13:30:13 +0200

    added all missing logic

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,5 +1,6 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-grid',
@@ -11,12 +12,26 @@ export class GridComponent implements OnInit {
   randomNumbers: number[] = [];
   evenValue: string[] = [];
 
+  timePassed: number = 0;
+  interval: any;
+  score: number = 0;
+
   constructor() {}
 
   ngOnInit(): void {
+    this.initializeRandomNumbers();
+    this.startTimer();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  initializeRandomNumbers(): void {
     for (let i = 0; i < 9; i++) {
-      const randomNum = Math.floor(Math.random() * 8);
-      this.randomNumbers.push(randomNum);
+      const randomNum = Math.floor(Math.random() * 10) + 90;
+        this.randomNumbers.push(randomNum);
+        this.score = this.score + randomNum;
     }
     for (let n = 0; n < 9; n++) {
       if (this.randomNumbers[n] % 2 == 0) {
@@ -27,15 +42,32 @@ export class GridComponent implements OnInit {
     console.log(this.evenValue);
   }
 
+  startTimer(): void {
+    this.interval = setInterval(() => {
+      this.timePassed++;}, 1000
+    )
+  }
+
+  endGame(message: string): void {
+    clearInterval(this.interval);
+    alert(message);
+  }
+
   sumValue: number = 0;
 
   drop(event: CdkDragDrop<number[]>): void {
     var prevIndex = event.previousIndex;
-    var currIndex = event.currentIndex +1;
+    var currIndex = this.calculateDropIndex(event);
 
     var prevValue = this.randomNumbers[prevIndex];
-    var currValue = this.randomNumbers[currIndex +1];
+    var currValue = this.randomNumbers[currIndex];
     this.sumValue = prevValue + currValue;
+
+    if (prevValue < currValue) {
+      this.sumValue = currValue - prevValue;
+    } else {
+      this.sumValue = prevValue + currValue;
+    }
 
     console.log('Dragged Tile Index:', prevIndex);
     console.log('Dragged Tile Value:', prevValue);
@@ -43,14 +75,28 @@ export class GridComponent implements OnInit {
     console.log('Dropped Tile Value:', currValue);
 
     // Prevent the tiles from moving
-    // event.item.reset();
+    event.item.reset();
 
     this.randomNumbers[prevIndex] = this.sumValue;
     this.randomNumbers[currIndex] = this.sumValue;
 
-    var prevIndex = 0;
-    var currIndex = 0;
+    this.score = this.randomNumbers.reduce((sum, value) => sum + value, 0);
+    if (this.score === 9) {
+      this.endGame('Congratulations! You cleared all tiles!')
+    }
   }
+
+  calculateDropIndex(event: CdkDragDrop<number[]>): number {
+    var dropPoint = event.dropPoint;
+    var gridElement = event.container.element.nativeElement;
+    var gridRect = gridElement.getBoundingClientRect();
+
+    var row = Math.floor((dropPoint.y - gridRect.top) / (gridRect.height / 3));
+    var col = Math.floor((dropPoint.x - gridRect.left) / (gridRect.width / 3));
+
+    return row * 3 + col;
+  }
+
 
 
 }
